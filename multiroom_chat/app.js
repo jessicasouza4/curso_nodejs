bd0a9f0f03d54ceb0fa3cb1bf8f95ee224f1bde4
Jessica Souza <gsik_bx@hotmail.com>
2017-08-09 20:00:02 -0300

    add socket.io e configurando servidor para o socket escutar na mesma porta

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -1,7 +1,10 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
 
+
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
 	console.log('Servidor online');
 })
+
+require('socket.io').listen(server);
