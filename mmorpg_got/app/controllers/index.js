135da3a85e4a11223111082c47c42d28fb39ea7a
Jessica Souza <gsik_bx@hotmail.com>
2017-10-17 21:02:25 -0200

    add variaveis de secao

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,21 @@
 module.exports.index = function(application, req, res){
 	res.render('index');
 }
+
+module.exports.autenticar = function(application, req, res){
+	
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+	req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render("index", {validacao: error});
+		return;
+	}
+
+	res.send('tudo okay')
+
+}
