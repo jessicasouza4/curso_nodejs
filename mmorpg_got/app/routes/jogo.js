78ab1d5414d3d411861747a40f6a98e226449ba1
Jessica Souza <gsik_bx@hotmail.com>
2017-11-16 21:14:39 -0200

    add acoes nos botoes pergaminho e suditos

--- mmorpg_got/app/routes/jogo.js
+++ mmorpg_got/app/routes/jogo.js
@@ -7,6 +7,14 @@ module.exports = function(application){
 		application.app.controllers.jogo.sair(application, req, res);
 	});
 
+	application.get('/suditos', function(req, res){
+		application.app.controllers.jogo.suditos(application, req, res);
+	});
+
+	application.get('/pergaminhos', function(req, res){
+		application.app.controllers.jogo.pergaminhos(application, req, res);
+	});
+
 
 }
 
