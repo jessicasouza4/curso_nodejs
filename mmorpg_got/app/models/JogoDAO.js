e047892346cb8fb90819957bc7660e90858ea963
Jessica Souza <gsik_bx@hotmail.com>
2017-11-16 20:36:49 -0200

    tornando as informacoes do jogador dinamicas

--- mmorpg_got/app/models/JogoDAO.js
+++ mmorpg_got/app/models/JogoDAO.js
@@ -9,13 +9,25 @@ JogoDAO.prototype.gerarParametros = function(usuario){
 				usuario: usuario,
 				moeda: 15,
 				suditos: 10,
-				temor: Math.floor(Math.randon() * 1000),
-				sabedoria: Math.floor(Math.randon() * 1000),
-				comercio: Math.floor(Math.randon() * 1000),
-				magia: Math.floor(Math.randon() * 1000)
+				temor: Math.floor(Math.random() * 1000),
+				sabedoria: Math.floor(Math.random() * 1000),
+				comercio: Math.floor(Math.random() * 1000),
+				magia: Math.floor(Math.random() * 1000)
 			});
 
 			mongoclient.close();
+		});
+	});
+}
+
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa){
+	this._connection.open( function(err, mongoclient){
+		mongoclient.collection("jogo", function(err, collection){
+			collection.find({usuario: usuario}).toArray(function(err, result){
+
+				res.render('jogo', {img_casa: casa, jogo: result[0]});
+				mongoclient.close();
+			});
 		});
 	});
 }
